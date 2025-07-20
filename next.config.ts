f845d28c7158f3c2172225df8577d7a570a45adf
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2025-07-20 08:57:12 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+      ignoreDuringBuilds: true,
+    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images:{
+    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  }
 };
 
 export default nextConfig;
